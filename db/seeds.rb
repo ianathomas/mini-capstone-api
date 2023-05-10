74134e133f58546b41cfd66043b0e2e810f9e5f1
IAN THOMAS <ianthomas9891@gmail.com>
2023-05-10 13:40:03 -0400

    migrated db and set product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#Create a Product model (singular!). Some important attributes include name, price (integer), image_url, and description.
+
