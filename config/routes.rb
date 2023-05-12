b357f5bb57b738c73de44873238f78a86158ba94
IAN THOMAS <ianthomas9891@gmail.com>
2023-05-12 14:58:51 -0400

    added model methods

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
 
   post "/products" => "products#create"
+
+  patch "/products/:id" => "products#update"
+
+  delete "/products/:id" => "products#destroy"
 end
